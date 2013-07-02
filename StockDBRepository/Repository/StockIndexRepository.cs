f0099af47927fa3b08159ed528df21ae0c9f2c84
Henry Lan <henrylanca@hotmail.com>
2013-07-02 08:29:00 -0400

    Data Repository

---
+++ StockDBRepository/Repository/StockIndexRepository.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StockDownloader.StockDBRepository
+{
+    public class StockIndexRepository
+    {
+        public void UpdateStockIndex(StockIndex stockIndex)
+        {
+            using (StockDataEntities context = new StockDataEntities())
+            {
+                StockIndex existingIndex = context.StockIndexes
+                    .Where(i => String.Compare(i.IndexName, stockIndex.IndexName, true) == 0).SingleOrDefault();
+
+                if (existingIndex != null)
+                {
+                    existingIndex.Description = stockIndex.Description;
+                }
+                else
+                {
+                    context.StockIndexes.Add(stockIndex);
+                }
+                
+                context.SaveChanges();
+            }
+        }
+
+    }
+}
