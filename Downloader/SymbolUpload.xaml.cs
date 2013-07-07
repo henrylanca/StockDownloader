b9ca475f62a75d16e3eacc637910a8eaaa12ee2d
Henry Lan <henrylanca@hotmail.com>
2013-07-07 16:16:49 -0400

    Upload Symbol

---
+++ Downloader/SymbolUpload.xaml.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace Downloader
+{
+    /// <summary>
+    /// Interaction logic for SymbolUpload.xaml
+    /// </summary>
+    public partial class SymbolUpload : Window
+    {
+        public SymbolUpload()
+        {
+            InitializeComponent();
+        }
+
+        private void btnUpload_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+    }
+}
---
+++ StockDBRepository/Repository/StockSymbolRepository.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StockDownloader.StockDBRepository
+{
+    public class StockSymbolRepository
+    {
+        public void UpdateSymbol(StockSymbol symbol)
+        {
+            using (StockDataEntities context = new StockDataEntities())
+            {
+                StockSymbol exsitingSymbol = context.StockSymbols
+                    .Where(s => String.Compare(s.Symbol, symbol.Symbol, true) == 0).SingleOrDefault();
+
+                if (exsitingSymbol != null)
+                {
+                    exsitingSymbol.StockName = symbol.StockName;
+                    exsitingSymbol.Sector = symbol.Sector;
+                    exsitingSymbol.ETF = symbol.ETF;
+                    exsitingSymbol.Country = symbol.Country;
+                    exsitingSymbol.HasFuture = symbol.HasFuture;
+                    exsitingSymbol.StartDate = symbol.StartDate;
+                    exsitingSymbol.EndDate = symbol.EndDate;
+                }
+                else
+                {
+                    context.StockSymbols.Add(symbol);
+                }
+
+                context.SaveChanges();
+            }
+        }
+
+
+    }
+}
