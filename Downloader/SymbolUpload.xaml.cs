de4326cda44eb7d3a800df03ce1ac4ca94677aa5
Henry Lan <henrylanca@hotmail.com>
2013-07-07 22:27:21 -0400

    Symbol Repository

--- Downloader/SymbolUpload.xaml.cs
+++ Downloader/SymbolUpload.xaml.cs
@@ -21,6 +21,15 @@ namespace Downloader
     /// </summary>
     public partial class SymbolUpload : Window
     {
+        class IndexComponents
+        {
+            public string Index { get; set; }
+            public List<StockSymbol> Symbols { get; set; }
+        }
+
+        private StockSymbolRepository _stockSymbolRepository = new StockSymbolRepository();
+        private StockIndexRepository _stockIndexReposotory = new StockIndexRepository();
+
         public SymbolUpload()
         {
             InitializeComponent();
@@ -38,9 +47,9 @@ namespace Downloader
             string[] lines = this.txtSymbols.Text.Replace("\r", "")
                 .Split(new[] {'\n'},StringSplitOptions.RemoveEmptyEntries);
 
-            List<StockSymbol> txtSymbols = new List<StockSymbol>();
-            
+            List<IndexComponents> indexComponentList = new List<IndexComponents>();
 
+            #region Parse all lines
             foreach (string line in lines)
             {
                 string[] items = line.Split(',');
@@ -56,6 +65,7 @@ namespace Downloader
                     int etf = 0;
                     int hasFuture= 0;
 
+                    string index = string.IsNullOrEmpty(items[0]) ? "NULL" : items[0];
                     StockSymbol symbol = new StockSymbol();
                     symbol.Symbol = items[1];
                     symbol.StockName = items[2];
@@ -68,8 +78,26 @@ namespace Downloader
                     int.TryParse(items[6], out hasFuture);
                     symbol.HasFuture = hasFuture>0;
 
+                    IndexComponents indexCom = indexComponentList
+                        .Where(i => string.Compare(i.Index, index, true) == 0).SingleOrDefault();
+
+                    if (indexCom == null)
+                    {
+                        indexCom = new IndexComponents() { Index = index };
+                        indexComponentList.Add(indexCom);
+                    }
+
+                    indexCom.Symbols.Add(symbol);
+
                 }
+            }
+            #endregion
+
+            foreach (IndexComponents indexCom in indexComponentList)
+            {
             }
         }
     }
+
+    
 }
